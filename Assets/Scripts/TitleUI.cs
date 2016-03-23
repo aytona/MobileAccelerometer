7913d5ad7a7bcdf407ecc47d799eb05d7fc16bda
Christopher Aytona <christopher.aytona@gmail.com>
2016-03-23 15:40:22 -0400

    Initial commit with a base title scene

---
+++ Assets/Scripts/TitleUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TitleUI : MonoBehaviour {
+
+	public void StartGame() {
+		SceneManager.LoadScene(1);
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
