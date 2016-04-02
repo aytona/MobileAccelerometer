089593fa3e9c629578778abd882d1bac3d0bbd77
aytona <christopher.aytona@gmail.com>
2016-04-02 00:15:17 -0400

    Leaderboard data, singleton, and arrayprefs added

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Data : Singleton<Data> {
+    #region Public
+
+    public void SetHighscoreArray(float score) {
+        int[] savedHighscore = new int[3];
+        int tempScore;
+        int paramScore = (int)score;
+        for (int i = 0; i < savedHighscore.Length; i++) {
+            if (savedHighscore[i] < paramScore) {
+                tempScore = savedHighscore[i];
+                savedHighscore[i] = paramScore;
+                paramScore = tempScore;
+            }
+        }
+
+        PlayerPrefsX.SetIntArray("Highscores", savedHighscore);
+    }
+
+    public void SetScore(int newScore) {
+        currentScore = newScore;
+        PlayerPrefs.SetInt("CurrentScore", currentScore);
+    }
+
+    public int GetScore() {
+        return currentScore;
+    }
+
+    public int[] GetHighscore() {
+        int[] savedHighscores = PlayerPrefsX.GetIntArray("Highscores", 0, 3);
+        return savedHighscores;
+    }
+
+    #endregion Public
+
+    #region Private
+
+    private int currentScore = 0;
+    private Data() { }
+
+    #endregion Private
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+    #region Public
+
+    public static T _Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    _instance = singleton.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+    public virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion Public
+
+    #region Private
+
+    private static T _instance;
+
+    #endregion Private
+}
