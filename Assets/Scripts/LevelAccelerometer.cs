7a4f3f18f5f720c968ec3c85ecbbccb72479c072
aytona <christopher.aytona@gmail.com>
2016-04-02 21:40:15 -0400

    Added levelaccelerometer script, need to test on device

---
+++ Assets/Scripts/LevelAccelerometer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+// Script that will rotate the game object according to the input acceleration
+public class LevelAccelerometer : MonoBehaviour
+{
+    void Update()
+    {
+        AccelerationMovement();
+    }
+
+    #region TiltMovement
+
+    private Vector3 AccelerationInput()
+    {
+        Vector3 acceleration = Input.acceleration;
+        if (acceleration.magnitude < -0.1f || acceleration.magnitude > 0.1f)
+        {
+            return acceleration;
+        }
+        return Vector3.zero;
+    }
+
+    private void AccelerationMovement()
+    {
+        transform.rotation = Quaternion.Euler(AccelerationInput());
+    }
+
+    #endregion TiltMovement
+}
