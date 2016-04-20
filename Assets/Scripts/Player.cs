9a63dfc575d0ef6add8c0e5c55d5239ab909ad16
Astalto <gowaoe@gmail.com>
2016-04-20 18:26:12 -0400

    Added coins, score, timer and game over state
    TODO: Fix camera, also make (and test) controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,15 +11,18 @@ public class Player : MonoBehaviour {
 
     private Rigidbody rb;
     private GameUI ui;
+    private int score;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         ui = FindObjectOfType<GameUI>();
+        score = 0;
     }
 
     void Update()
     {
+        rb.AddForce(Vector3.forward * 5);
         if (Input.GetMouseButtonDown(0) && IsGrounded())
         {
             Jump();
@@ -49,5 +52,25 @@ public class Player : MonoBehaviour {
     	ui.StartGoal();
     	yield return new WaitForSeconds(_delay);
     	ui.ToMain();
+    }
+
+    private void increaseScore(int amount)
+    {
+        score += amount;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Coin"))
+        {
+            increaseScore(100);
+            Debug.Log(score);
+            Destroy(other.gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/TimerCount.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerCount : MonoBehaviour {
+    public Text timerText;
+    public GameObject loseOverlay;
+    private float timeLeft;
+
+	// Use this for initialization
+	void Start () {
+        loseOverlay.SetActive(false);
+        timeLeft = 300.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        timerText.text = "Time Left: " + (int)timeLeft;
+        if(timeLeft <= 0)
+        {
+            //Trigger lose state
+            Time.timeScale = 0;
+            loseOverlay.SetActive(true);
+        }
+	}
+}
